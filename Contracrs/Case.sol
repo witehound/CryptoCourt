4f5516cf6eb57263121fd6ae875fbe3d529f8946
InventedMost <netvid00@gmail.com>
2022-06-03 23:56:19 +0100

    make argument added

--- Contracrs/Case.sol
+++ Contracrs/Case.sol
@@ -9,6 +9,14 @@ contract Case {
     address public Plaintiff;
     address public Defendant;
     State public state;
+    argument[] public arguments;
+
+    mapping(address => uint256) public argumentsCount;
+
+    struct argument {
+        string argument;
+        address maker;
+    }
 
     enum State {
         Created,
@@ -26,5 +34,33 @@ contract Case {
         Plaintiff = msg.sender;
         Defendant = _deff;
         state = State.Created;
+    }
+
+    function becomeJudge() public {
+        require(msg.sender != Plaintiff, "Plattiff cant become judge");
+        require(msg.sender != Defendant, "Defendant cant become judge");
+        Judge = msg.sender;
+        state = State.Started;
+    }
+
+    function whoIsJudge() public view returns (address) {
+        return Judge;
+    }
+
+    function makeArgument(string memory _argument) public {
+        require(
+            msg.sender == Plaintiff || msg.sender == Defendant,
+            "only plantiff or defendant can make argument"
+        );
+        require(
+            argumentsCount[msg.sender] <= 3,
+            "only allowed to make 3 arguments"
+        );
+        argument memory newArgument = argument({
+            argument: _argument,
+            maker: msg.sender
+        });
+        argumentsCount[msg.sender] += 1;
+        arguments.push(newArgument);
     }
 }
