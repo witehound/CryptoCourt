976a2d9ae652af15e5e792b81097ff8c93da491b
InventedMost <netvid00@gmail.com>
2022-06-04 21:36:13 +0100

    new judge features added

--- Contracrs/Case.sol
+++ Contracrs/Case.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.7.0 <0.9.0;
-import "./Jury.sol";
 
 contract Case {
     string public Title;
@@ -8,11 +7,16 @@ contract Case {
     address public Judge;
     address public Plaintiff;
     address public Defendant;
-    State public CaseState = State.NotCreated;
+    State public CaseState = State.Created;
+
     argument[] public arguments;
 
-    mapping(address => uint256) public argumentsCount;
+    uint256[2] public votes = [0, 0];
+    uint256 public jurorCount = 0;
+    mapping(address => bool) public JuryMap;
+    mapping(address => bool) public voted;
 
+    mapping(address => uint256) public argumentsCount;
     struct argument {
         string argument;
         address maker;
@@ -38,6 +42,8 @@ contract Case {
         CaseState = State.Created;
     }
 
+    //case functions
+
     function becomeJudge() public {
         require(msg.sender != Plaintiff, "Plattiff cant become judge");
         require(msg.sender != Defendant, "Defendant cant become judge");
@@ -48,6 +54,46 @@ contract Case {
     function whoIsJudge() public view returns (address) {
         return Judge;
     }
+
+    function startTrial() public {
+        require(CaseState == State.Created, "trial cant start just yet");
+        require(msg.sender == Judge, "only judge can start trial");
+        CaseState = State.Started;
+    }
+
+    //juorr functions
+
+    function becomeAJuror() public {
+        require(jurorCount <= 12, "No more spot for a new juror");
+        require(!JuryMap[msg.sender], "You are already registered as a juror");
+        require(
+            msg.sender != Plaintiff && msg.sender != Defendant,
+            "only plantiff or defendant can make argument"
+        );
+        require(CaseState == State.Started, "wait till trial has started");
+        JuryMap[msg.sender] = true;
+        jurorCount++;
+    }
+
+    function checkAJuror(address Juror) public view returns (bool) {
+        return JuryMap[Juror];
+    }
+
+    function jurorVoteYes() public {
+        require(JuryMap[msg.sender] == true, "only a juror can vote");
+        require(voted[msg.sender] == false, "already voted");
+        require(CaseState == State.Started, "wait till trial has started");
+        votes[0]++;
+    }
+
+    function jurorVoteNo() public {
+        require(JuryMap[msg.sender] == true, "only a juror can vote");
+        require(voted[msg.sender] == false, "already voted");
+        require(CaseState == State.Started, "wait till trial has started");
+        votes[1]++;
+    }
+
+    //arguement functions
 
     function makeArgument(string memory _argument) public {
         require(
@@ -65,5 +111,11 @@ contract Case {
         });
         argumentsCount[msg.sender] += 1;
         arguments.push(newArgument);
+    }
+
+    //views
+
+    function getVotesCount() public view returns (uint256[2] memory) {
+        return votes;
     }
 }
